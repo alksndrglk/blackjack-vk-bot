3ad8268d85b4c57c443e53cdd33df25c3fa2f551
Aleksandr Goliak <60971202+alksndrglk@users.noreply.github.com>
2022-09-16 10:24:46 +0300

    Feature 2 (#2)
    * Keyboards added

---
+++ app/store/game/keyboards.py
@@ -0,0 +1,132 @@
+GREETING = {
+    "one_time": True,
+    "buttons": [
+        [
+            {
+                "action": {
+                    "type": "callback",
+                    "payload": '{"command": "start"}',
+                    "label": "Старт",
+                },
+                "color": "positive",
+            },
+        ]
+    ],
+}
+
+
+BID = {
+    "one_time": True,
+    "buttons": [
+        [
+            {
+                "action": {
+                    "type": "callback",
+                    "payload": '{"command": "bid_10"}',
+                    "label": "10",
+                },
+                "color": "primary",
+            },
+            {
+                "action": {
+                    "type": "callback",
+                    "payload": '{"command": "bid_20"}',
+                    "label": "20",
+                },
+                "color": "primary",
+            },
+            {
+                "action": {
+                    "type": "callback",
+                    "payload": '{"command": "bid_50"}',
+                    "label": "50",
+                },
+                "color": "primary",
+            },
+            {
+                "action": {
+                    "type": "callback",
+                    "payload": '{"command": "bid_100"}',
+                    "label": "100",
+                },
+                "color": "primary",
+            },
+        ],
+        [
+            {
+                "action": {
+                    "type": "callback",
+                    "payload": '{"command": "cancel_bid"}',
+                    "label": "Отмена",
+                },
+                "color": "primary",
+            },
+            {
+                "action": {
+                    "type": "callback",
+                    "payload": '{"command": "finished_bidding"}',
+                    "label": "Ставка",
+                },
+                "color": "positive",
+            },
+        ]
+    ],
+}
+
+DECISION_MAKING = {
+    "one_time": True,
+    "buttons": [
+        [
+            {
+                "action": {
+                    "type": "callback",
+                    "payload": '{"command": "double"}',
+                    "label": "Удвоить",
+                },
+                "color": "secondary",
+            },
+            {
+                "action": {
+                    "type": "callback",
+                    "payload": '{"command": "hit"}',
+                    "label": "Ещё",
+                },
+                "color": "positive",
+            },
+        ],
+        [
+            {
+                "action": {
+                    "type": "callback",
+                    "payload": '{"command": "stand"}',
+                    "label": "Хватит",
+                },
+                "color": "primary",
+            },
+        ]
+    ],
+}
+
+END = {
+    "one_time": True,
+    "buttons": [
+        [
+            {
+                "action": {
+                    "type": "callback",
+                    "payload": '{"command": "leave"}',
+                    "label": "Закончить",
+                },
+                "color": "primary",
+            },
+            {
+                "action": {
+                    "type": "callback",
+                    "payload": '{"command": "continue"}',
+                    "label": "Играть Заново",
+                },
+                "color": "positive",
+            },
+        ]
+    ],
+}
--- app/store/game/state.py
+++ app/store/game/state.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 import typing
 from app.game.models import GameState
+from app.store import Store
+from app.store.bot.dataclassess import Message
 
 
 class StateProcessor:
@@ -9,10 +11,10 @@ class StateProcessor:
     @classmethod
     def register_handler(cls, command_type: GameState) -> typing.Callable:
         def decorator(function: typing.Callable):
-            cls.cls_handlers[command_type] = function
+            cls.handlers[command_type.value] = function
             return function
 
         return decorator
 
-    async def process(self, command_type: GameState, message: str):
-        await self.cls_handlers[command_type](message)
+    async def process(self, command_type: GameState, message: Message, func):
+        await self.handlers[command_type](message, func)
