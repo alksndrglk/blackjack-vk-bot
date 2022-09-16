6f0df08f11d2be947f96e5f729eabbcbff3d58b4
Aleksandr Goliak <alksndrglk@gmail.com>
2022-09-16 10:19:16 +0300

    Nice unpacking with dataclasses

--- app/store/vk_api/accessor.py
+++ app/store/vk_api/accessor.py
@@ -8,7 +8,7 @@ from aiohttp.client import ClientSession
 
 from app.base.base_accessor import BaseAccessor
 from .poller import Poller
-from .dataclasses import Update, UpdateObject, VkUser, Message
+from .dataclasses import Update, VkUser, Message
 
 if typing.TYPE_CHECKING:
     from app.web.app import Application
@@ -68,7 +68,7 @@ class VkApiAccessor(BaseAccessor):
                 self.ts = data["ts"]
                 self.logger.info(self.server)
 
-    async def poll(self):
+    async def poll(self) -> Optional[list[Update]]:
         async with self.session.get(
             self._build_query(
                 host=self.server,
@@ -86,36 +86,9 @@ class VkApiAccessor(BaseAccessor):
                 self.logger.info(data)
                 self.ts = data["ts"]
                 raw_updates = data.get("updates", [])
-                updates = []
-                for update in raw_updates:
-                    peer_id = update["object"].get("peer_id", None)
-                    user_id = update["object"].get("user_id", None)
-                    _type = update["type"]
-                    updates.append(
-                        Update(
-                            type=_type,
-                            object=UpdateObject(
-                                id=update["object"].get("message", {}).get("id"),
-                                peer_id=peer_id
-                                if peer_id
-                                else update["object"].get("message", {}).get("peer_id"),
-                                user_id=user_id
-                                if user_id
-                                else update["object"].get("message", {}).get("from_id"),
-                                body=update["object"].get("message", {}).get("text"),
-                                action=update["object"]
-                                .get("message", {})
-                                .get("action", {}),
-                                payload=update["object"].get("payload", {}),
-                                obj=update["object"]
-                                if _type == "message_event"
-                                else {},
-                            ),
-                        )
-                    )
-                return updates
+                return [Update.from_dict(upd) for upd in raw_updates]
 
-    async def get_conversation_members(self, peer_id) -> Optional[List[VkUser]]:
+    async def get_conversation_members(self, peer_id) -> Optional[list[VkUser]]:
         async with self.session.get(
             self._build_query(
                 API_PATH,
--- app/store/vk_api/dataclasses.py
+++ app/store/vk_api/dataclasses.py
@@ -4,20 +4,76 @@ from typing import Optional, Union
 
 
 @dataclass
-class UpdateObject:
-    id: int
+class Payload:
+    command: str
+
+    @classmethod
+    def from_dict(cls, data: dict) -> Payload:
+        return cls(command=data.get("command"))
+
+
+@dataclass
+class Action:
+    type_: str
+    member_id: int
+
+    @classmethod
+    def from_dict(cls, data: dict) -> Action:
+        return cls(type_=data.get("type"), member_id=data.get("member_id"))
+
+
+@dataclass
+class UpdateMessageObject:
     peer_id: int
     user_id: int
     body: str
-    action: dict = field(default_factory=dict)
-    payload: dict = field(default_factory=dict)
-    obj: dict = field(default_factory=dict)
+    action: Action
+
+    @classmethod
+    def from_dict(cls, data: dict) -> Action:
+        msg = data.get("message")
+        return cls(
+            peer_id=msg.get("peer_id"),
+            user_id=msg.get("from_id"),
+            body=msg.get("text"),
+            action=Action.from_dict(msg.get("action", {})),
+        )
+
+
+@dataclass
+class UpdateEventObject:
+    peer_id: int
+    user_id: int
+    event_id: str
+    payload: Payload
+
+    @classmethod
+    def from_dict(cls, data: dict) -> Action:
+        return cls(
+            peer_id=data.get("peer_id"),
+            user_id=data.get("from_id"),
+            event_id=data.get("event_id"),
+            payload=Payload.from_dict(data.get("payload", {})),
+        )
 
 
 @dataclass
 class Update:
-    type: str
-    object: UpdateObject
+    type_: str
+    object: Union[UpdateMessageObject, UpdateEventObject]
+
+    upd_factory = {
+        "message_new": UpdateMessageObject,
+        "message_event": UpdateEventObject,
+    }
+
+    @classmethod
+    def from_dict(cls, data: dict) -> Update:
+        upd_type = data.get("type")
+        obj_cls = cls.upd_factory.get(upd_type)
+        if obj_cls is None:
+            return
+        return cls(type_=upd_type, object=obj_cls.from_dict(data.get("object")))
 
 
 @dataclass
